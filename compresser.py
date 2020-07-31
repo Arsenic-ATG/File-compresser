c056976149597983fd61eab239c3dfce903c439c
Ankur <54987647+Arsenic-ATG@users.noreply.github.com>
2020-07-31 13:21:26 +0530

    Handel the zipfile name

--- compresser.py
+++ compresser.py
@@ -2,9 +2,19 @@
 
 import os,zipfile
 
-# Backup the entire contents of "folder" into a ZIP file.
+def backup_to_zip(folder):
+	# Backup the entire contents of "folder" into a ZIP file.
+	folder = os.path.abspath(folder)
 
-# Figure out the filename this code should use based on what files already exist.
+	# Figure out the filename this code should use based on what files already exist.
+	number = 1;
+	while True:
+		zipFile_name = os.path.basename(folder) + '_' + str(number) + '.zip'
+
+		if not os.path.exist(zipFile_name):
+			break
+
+		number + number + 1
 
 # TODO: Create the ZIP file.
 
