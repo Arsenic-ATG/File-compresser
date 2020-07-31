ba955ab2e59ed0ef51815dc560564e2b0a0df1c9
Ankur <54987647+Arsenic-ATG@users.noreply.github.com>
2020-07-31 13:16:12 +0530

    Maped out the porject file

---
+++ compresser.py
@@ -0,0 +1,13 @@
+# backupToZip.py - Copies an entire folder and its contents into a ZIP file whose filename increments.
+
+import os,zipfile
+
+# Backup the entire contents of "folder" into a ZIP file.
+
+# Figure out the filename this code should use based on what files already exist.
+
+# TODO: Create the ZIP file.
+
+# TODO: Walk the entire folder tree and compress the files in each folder.
+
+print('Done.')
